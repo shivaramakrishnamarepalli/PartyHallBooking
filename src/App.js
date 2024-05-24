31584a22c4a6461bbda83c826c8ce48e33aca8f5
priyanshusaineni <priyanshusaineni2004@gmail.com>
2024-05-24 16:44:42 +0530

    added styling to cards on user dashboard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import "./App.css";
-import CardsDisplay from "./components/CardsDisplay";
+import './App.css'
+import CardsDisplay from './components/CardsDisplay'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 function App() {
   return (
     <>
       <h1>React App</h1>
-      <CardsDisplay />
+      {/* <CardsDisplay /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/cards" element={<CardsDisplay />} />
+          <Route path="/admin/editHall" element={<EditAndDelteHall />} />
+          <Route path="/admin/addHall" element={<RequestToAddHall />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -1,27 +1,58 @@
 import img_path from '../images/logo192.png'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/index.css'
-
-function Card({ imageUrl, name, location, rentalCost, maxCapacity }) {
+import rating1 from '../images/rating1.png'
+import addressIcon from '../images/address-icon.png'
+import { FaRupeeSign } from 'react-icons/fa'
+import { FaBuilding } from 'react-icons/fa'
+import { IoPerson } from 'react-icons/io'
+function Card({ imageUrl, name, address, rental_cost, rating, capacity }) {
   return (
     <>
+      {}
       <div
-        className="card p-3 m-5 border-0 card-hover"
-        style={{ width: '22rem' }}
+        className="card p-3 m-5 border-0 card-hover "
+        style={{ width: '25rem' }}
       >
-        <img src={img_path} className="card-img-top img-fluid" alt="..." />
+        <img
+          src={img_path}
+          className="card-img-top img-fluid"
+          alt="..."
+          width={'10px'}
+        />
         <div className="card-body">
           <p className="card-text">
             <div className="d-flex justify-content-between">
-              <span>Hall Name</span>
-              <span>Rating</span>
+              <span>
+                <h5>
+                  <FaBuilding />
+                  {name}
+                </h5>
+              </span>
+              <span>
+                {' '}
+                <h5>
+                  <img src={rating1} width={'22rem'} />
+                  <span> {rating + '.0'}</span>
+                </h5>
+              </span>
             </div>
             <div>
-              <span>Hall location </span>
+              <span>
+                <img src={addressIcon} width={'22rem'} />
+                {address}
+              </span>
             </div>
             <div className="d-flex justify-content-between">
-              <span>Hall rentalCost</span>
-              <span>Hall rentalCost</span>
+              <span>
+                {' '}
+                <FaRupeeSign />
+                {rental_cost}
+              </span>
+              <span>
+                {/* <IoPerson /> */}
+                {capacity}
+              </span>
             </div>
           </p>
         </div>
