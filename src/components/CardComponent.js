492a600b401c719862c279b64db4b7487c63e2fc
priyanshusaineni <priyanshusaineni2004@gmail.com>
2024-05-24 15:41:44 +0530

    added card component

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -1,15 +1,43 @@
-function Card() {
+import img_path from '../images/logo192.png'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../styles/index.css'
+
+function Card({ imageUrl, name, location, rentalCost, maxCapacity }) {
   return (
     <>
-      <div className="card" style={{ width: '18rem' }}>
-        <img src="" className="card-img-top" alt="..." />
+      <div
+        className="card p-3 m-5 border-0 card-hover"
+        style={{ width: '22rem' }}
+      >
+        <img src={img_path} className="card-img-top img-fluid" alt="..." />
         <div className="card-body">
           <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            <div className="d-flex justify-content-between">
+              <span>Hall Name</span>
+              <span>Rating</span>
+            </div>
+            <div>
+              <span>Hall location </span>
+            </div>
+            <div className="d-flex justify-content-between">
+              <span>Hall rentalCost</span>
+              <span>Hall rentalCost</span>
+            </div>
           </p>
         </div>
       </div>
+      {/* <div className="card p-3 m-3" style={{ width: '22rem' }}>
+        <img src={imageUrl} className="card-img-top" alt={name} />
+        <div className="card-body">
+          <h5 className="card-title">{name}</h5>
+          <p className="card-text">1{location}</p>
+          <p className="card-text">1{rentalCost}</p>
+          <p className="card-text">2{maxCapacity}</p>
+        </div>
+        <div className="card-footer">
+          <button className="btn btn-primary">Book Now</button>
+        </div> 
+      </div>*/}
     </>
   )
 }
