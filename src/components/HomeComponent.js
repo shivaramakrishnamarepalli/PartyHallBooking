0534b0d4ca934448e8873171b3c0e1ea9e395de4
priyanshusaineni <priyanshusaineni2004@gmail.com>
2024-07-02 21:21:02 +0530

    adding my bookings button

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,11 +1,24 @@
 import Navbar from "./Navbar";
 import Center from "./Center";
 import Navbar1 from "./Navbar";
+import { useNavigate } from "react-router-dom";
 
 function HomeComponent() {
+  const navigate = useNavigate();
+  function handleBooking() {
+    navigate("/user/bookings");
+  }
   return (
     <>
       <Navbar1 />
+      <div className="d-flex justify-content-center">
+        <button
+          className="btn btn-success p-2 mt-5 text-center fs-4"
+          onClick={handleBooking}
+        >
+          My bookings
+        </button>
+      </div>
       <Center />
     </>
   );
